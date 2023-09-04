7fc82f97db7baf2dd5f3d2f87f1561b851b56e70
Matheus Gaudencio do Rêgo <matheusgr@users.noreply.github.com>
2023-09-04 17:24:24 -0300

    Add plausible tags.

--- components/GlobalTags.tsx
+++ components/GlobalTags.tsx
@@ -8,6 +8,8 @@ function GlobalTags() {
 
       {/* Tailwind v3 CSS file */}
       <link href={asset("/styles.css")} rel="stylesheet" />
+      
+      <script defer data-domain="plausible.deco.site" src="https://plausible.io/js/script.js"></script>
 
       {/* Web Manifest */}
       <link rel="manifest" href={asset("/site.webmanifest")} />
